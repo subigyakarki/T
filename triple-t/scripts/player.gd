f916f031c52cd02b2a407cc592f9885478d72c10
soobiᐢ..ᐢ <subigyakarki@gmail.com>
2026-04-21 18:10:43 -0400

    Add missing scene files

--- triple-t/scripts/player.gd
+++ triple-t/scripts/player.gd
@@ -2,20 +2,17 @@ extends CharacterBody2D
 
 
 const SPEED = 130.0
-const JUMP_VELOCITY = -300.0
+const JUMP_VELOCITY = -600.0
 
+@onready var anim = $AnimatedSprite2D
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
-	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("ui_left", "ui_right")
 	if direction:
 		velocity.x = direction * SPEED
@@ -23,3 +20,16 @@ func _physics_process(delta: float) -> void:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
+
+	if direction == 0:       
+		anim.play("idle")
+	else:
+		anim.play("run")
+	if direction < 0:
+		anim.flip_h = true
+	elif  direction > 0:
+		anim.flip_h = false
+		
+		
+		
+	
